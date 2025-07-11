aae054988f1312a9545612c0c892590020996d4c
Anup Maurya <anup.maurya484@gmail.com>
2025-07-11 11:09:41 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center w-full mt-14">
+        <button type="button" className="px-4 py-1 font-semibold text-white bg-blue-500 border border-blue-500 rounded-md">Upload image</button>
+      </div>
+    </>
+  )
+}
+
+export default App
